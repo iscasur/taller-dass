2af54355084aa6b51d582a925c813db60b14f8e8
Israel Castro <israel.castro@gmail.com>
2021-04-09 12:42:18 -0500

    [Add] countdown

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+let countDate = new Date('Apr 15, 2021 11:00:00').getTime();
+
+function workshopStarts() {
+  let now = new Date().getTime();
+  let gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  let d = Math.floor(gap / day);
+  let h = Math.floor((gap % day) / hour);
+  let m = Math.floor((gap % hour) / minute);
+  let s = Math.floor((gap % minute) / second);
+
+  document.getElementById('day').innerText = d;
+  document.getElementById('hour').innerText = h;
+  document.getElementById('minute').innerText = m;
+  document.getElementById('second').innerText = s;
+}
+
+setInterval(function () {
+  workshopStarts();
+}, 1000);
